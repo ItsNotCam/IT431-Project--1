67db9c624b634d34a4f668ad5c3bac793f8fc699
ItsNotCam <samspwrsd@gmail.com>
2025-03-13 00:20:50 -0400

    replace home background image with a new webp format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default async function Home() {
 	return (
 		<main>
-			<img src="/homebg.jpg" className='absolute top-0 left-0 w-full h-full -z-10 object-cover'></img>
+			<img src="/homebg.webp" className='absolute top-0 left-0 w-full h-full -z-10 object-cover'></img>
 			<div className='absolute top-1/2 left-1/2 -translate-1/2 text-gray-800 flex items-center justify-center w-full flex-col gap-2'>
 				<h1 className='text-5xl font-bold'>The way car</h1>
 				<h1 className='text-5xl font-bold'>buying should be</h1>
